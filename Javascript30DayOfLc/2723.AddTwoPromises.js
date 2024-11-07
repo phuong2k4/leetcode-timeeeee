184fd6f4a230c6c0a3a3602d4cf1bba2197a6ea6
phuong2k4 <phuongsepfg@gmail.com>
2024-11-07 22:36:06 +0700

    2723-AddTwoPromises

--- Javascript30DayOfLc/2723.AddTwoPromises.js
+++ Javascript30DayOfLc/2723.AddTwoPromises.js
@@ -3,8 +3,9 @@ const promise1 = new Promise(resolve => setTimeout(()=>{resolve(2),20}));
 const promise2 = new Promise(resolve => setTimeout(()=>{resolve(5),60}));
 
 const addTwoPromises = async (promise1,promise2)=>{
-    const [val1, val2] = await Promise.all(promise1,promise2);
+    const [val1, val2] = await Promise.all([promise1,promise2]);
     return val1 + val2; 
 }
 
-addTwoPromises(promise1,promise2).then(console.log)
+addTwoPromises(Promise.resolve(2),Promise.resolve(20))
+.then(console.log)
