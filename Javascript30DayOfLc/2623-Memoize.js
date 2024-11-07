4e59436aed5d295efdf3f7d35987417ed88628fc
phuong2k4 <phuongsepfg@gmail.com>
2024-11-07 19:26:05 +0700

    MemoizeP2

--- Javascript30DayOfLc/2623-Memoize.js
+++ Javascript30DayOfLc/2623-Memoize.js
@@ -5,14 +5,15 @@ const memoize = (func)=>{
         if(key in caches){
             return caches[key];
         }
-
-        const result = func(...args)
+        const result = func(...args);
         caches[key] = result;
         return result;
     }
 } 
 
-const sum = (a,b)=>{a+b};
+const sum = (a,b)=>{
+    return a+b;
+};
 const factorial = (n) => {
     if(n == 2 ){
         return 2;
@@ -20,12 +21,26 @@ const factorial = (n) => {
     return n * factorial(n-1);
 }
 const fibonacci = (n)=>{
-    if(n <= 1 ){
-        return 1;
+    if (n === 0 || n === 1){
+        return n;
     }
     return fibonacci(n-1) + fibonacci(n-2);
 }
 
-const value = [[2,2], [2,2], [], [1,2],[]];
+console.log(fibonacci(4))
+
+const valueSum = [[2,2], [2,2], [], [1,2],[]];
 const memoizeSum = memoize(sum);
 console.log(memoizeSum(2,2));
+console.log(memoizeSum(2,2));
+
+const valueFac = [[2],[3],[2],[],[3],[]];
+const memoFactorial = memoize(factorial);
+console.log(memoFactorial(2));
+console.log(memoFactorial(3));
+console.log(memoFactorial(2));
+
+const valueFibo = [[5],[]];
+const memoFibonacci = memoize(fibonacci);
+console.log(memoFibonacci(5));
+console.log(memoFibonacci());
