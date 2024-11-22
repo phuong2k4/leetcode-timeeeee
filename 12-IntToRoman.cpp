f759f82b85499e1b48257c3e4c3dce13c4f33900
phuong2k4 <phuongsepfg@gmail.com>
2024-11-21 16:42:52 -0800

    12-IntToRoman

---
+++ 12-IntToRoman.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+
+class Solution{
+    public:
+        string intToRoman(int __number__){
+            const vector<pair<int , string>> valueSymbol{
+                {1000,"M"},{900,"CM"},{500,"D"},
+                {400,"CD"},{100,"C"},{90,"XC"},
+                {50,"L"},{40,"XL"},{10,"X"},
+                {9,"IX"},{50,"V"},{40,"IV"},
+                {1,"I"}
+            };
+            string result;
+            for(const auto&[value, symbol] : valueSymbol){
+                if (__number__ == 0){
+                    break;
+                }
+                while(__number__ >= value){
+                    result += symbol;
+                    __number__ -= value;
+                }
+            }
+            return result;
+        }
+};
