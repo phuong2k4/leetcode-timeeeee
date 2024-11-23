90b0215f25502af12833bd91216ad0ee302904c6
phuong2k4 <phuongsepfg@gmail.com>
2024-11-22 16:39:20 -0800

    13-RomanToInt

---
+++ 13-RomanToInt.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#include<iostream>
+
+using namespace std;
+
+class Solution{
+    int romanToInt(string s){
+        unordered_map<char, int> roman = {
+            {'I',1},{'V',5},{'X',10},
+            {'L',50},{'C',100},{'D',500},
+            {'M',1000}
+        };
+        int res = 0;
+        for(int i = 0 ;i< s.size(); i++){
+            if (roman[s[i]] < roman[s[i+1]]){
+                res -= roman[s[i]];
+            }else{
+                res+= roman[s[i]];
+            }
+        }
+        return res + roman[s[s.size() - 1]];
+    }
+};
