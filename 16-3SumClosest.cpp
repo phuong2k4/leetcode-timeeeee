29765b53fc043ddca1e3d90525b9bc5338ea4640
phuong2k4 <phuongsepfg@gmail.com>
2024-12-03 07:28:14 -0800

    16-3SumClosest

---
+++ 16-3SumClosest.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution{
+    public:
+        int threeSumClosest(vector <int>& nums, int target){
+            sort(nums.begin(), nums.end());
+            int closest_Sum = INT_MAX / 2;
+
+            for(int i = 0; i < nums.size() - 2;++i){
+                int left = i + 1, right = nums.size() - 1;
+                while(left < right){
+                    int current_sum = nums[i] + nums[left] + nums[right];
+                    if(abs(current_sum - target) < abs(closest_Sum - target)){
+                        closest_Sum = current_sum;
+                    }
+                    if(current_sum < target){
+                        ++left;
+                    }else if(current_sum > target){
+                        --right;
+                    }else{
+                        return current_sum;
+                    }
+                }
+            }
+            return closest_Sum;
+        }
+};
