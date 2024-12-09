9e220de66285e4399e7b7693ba036b27f9bf5fab
phuong2k4 <phuongsepfg@gmail.com>
2024-12-09 08:36:59 -0800

    17-LetterCombinations

--- test.js
+++ test.js
@@ -87,22 +87,24 @@
 // console.log(longestSubstringWthRepeatChar1("abcabcbb"));
 
 
-const intToRoman = (nums)=>{
-    const _valueOfSymbol_ = {
-        "M": 1000,"CM": 900,"D":500,"CD":400,"C":100,
-        "XC": 90,"L": 50,"XL":40,"X":10,"IX":9,
-        "V": 5,"IV": 4, "I": 1
-    }
-    let result;
-    while(nums){
-        Object.keys(_valueOfSymbol_).map(key=>{
-            while(nums >= _valueOfSymbol_[key]){
-                result += key;
-                nums -= _valueOfSymbol_[key];
-            }
-        })
-    }
-    return result;
-}
+// const intToRoman = (nums)=>{
+//     const _valueOfSymbol_ = {
+//         "M": 1000,"CM": 900,"D":500,"CD":400,"C":100,
+//         "XC": 90,"L": 50,"XL":40,"X":10,"IX":9,
+//         "V": 5,"IV": 4, "I": 1
+//     }
+//     let result;
+//     while(nums){
+//         Object.keys(_valueOfSymbol_).map(key=>{
+//             while(nums >= _valueOfSymbol_[key]){
+//                 result += key;
+//                 nums -= _valueOfSymbol_[key];
+//             }
+//         })
+//     }
+//     return result;
+// }
 
-console.log(intToRoman(1994));
+// console.log(intToRoman(1994));
+
+
